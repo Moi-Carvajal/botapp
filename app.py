5735ed2cb9c6c14cc93f95ec5d2e756ef1ea221c
Moises Gustavo Carvajal Cruz <moi.carvcruz@gmail.com>
2021-02-05 05:58:18 -0600

    Update app

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 import transformers
 import torch.nn as nn
 from torch.nn import CrossEntropyLoss
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template, url_for
 import pandas as pd
 import torch
 
@@ -19,12 +19,12 @@ t_model = torch.load('Modelo_final',map_location=torch.device('cpu'))
 db=pd.read_excel("db.xlsx")
 app= Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return 'Ready!'
-
-@app.route("/predecir", methods=["POST"])
-def predecir(tokenizer=tokenizer,t_model=t_model,db=db):
+    return render_template('home.html')
+    # return "listo"
+@app.route("/getanswer", methods=["POST"])
+def getanswer(tokenizer=tokenizer,t_model=t_model,db=db):
     json_doc=request.get_json(force=True)
     # if not json_doc:
     #   return jsonify(error="request body cannot be empty"), 400
@@ -45,8 +45,44 @@ def predecir(tokenizer=tokenizer,t_model=t_model,db=db):
         v_f.append(db['respuesta'][i])
       value=v_f
     #---------------------------------------------------------------------------
-    resp={"Respuesta":value}
+    resp={"respuesta":value}
     return jsonify(resp)
+@app.route("/getlabel", methods=["POST"])
+def getlabel(tokenizer=tokenizer,t_model=t_model,db=db):
+    json_doc=request.get_json(force=True)
+    # if not json_doc:
+    #   return jsonify(error="request body cannot be empty"), 400
+    sentence=json_doc.get('oracion')
+    #---------------------------------------------------------------------------
+    #tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
+    encoding = tokenizer(str(sentence.lower()), return_tensors='pt', padding=True, truncation=True)
+    texto_ids = encoding['input_ids']
+    t_model.eval()
+    predict = t_model(texto_ids)
+    Etiqueta= torch.argmax(predict[0]).item()
+    resp={"etiqueta":Etiqueta}
+    return jsonify(resp)
+
+@app.route("/temas")
+def temas(db=db):
+    lista_temas=db['tema']
+    lista_etiqueta=db['etiqueta']
+    return render_template("temas.html", len = len(lista_etiqueta), lista_etiqueta = lista_etiqueta, lista_temas = lista_temas) 
+    # dic={}
+    # for i in db['etiqueta']:
+    #   #print(lista_temas[i],lista_etiqueta[i])
+    #   dic[lista_temas[i]] =lista_etiqueta[i]
+    # return str(dic)
+@app.route("/respuestas")
+def respuestas(db=db):
+    lista_temas=db['tema']
+    lista_respuesta=db['respuesta']
+    return render_template("temas.html", len = len(lista_temas), lista_etiqueta = lista_respuesta, lista_temas = lista_temas)
+    # dic={}
+    # for i in db['etiqueta']:
+    #   #print(lista_temas[i],lista_respuesta[i])
+    #   dic[lista_temas[i]] =lista_respuesta[i]
+    # return str(dic)
 
 if __name__ == '__main__':
     app.run(debug=True)
